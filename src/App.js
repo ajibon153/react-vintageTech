facca73b7e207ec3eee2309e25051338dfd2f2ec
Aji Muhammad Fauji <47517952+ajibon153@users.noreply.github.com>
2020-12-23 10:25:16 +0700

    first move

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
+import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import About from './pages/About';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import ProductDetails from './pages/ProductDetails';
+import Products from './pages/Products';
+import Header from './components/Header';
+import Alert from './components/Alert';
+import PrivateRoute from './components/PrivateRoute';
 
-// // 3 Vintage tech store
-import Clock from './clockOwn/Index';
-import Vintage from './3VintageTech/App';
-import ProductProvider from './3VintageTech/context/products';
-import { CartProvider } from './3VintageTech/context/cart';
-import { UserProvider } from './3VintageTech/context/user';
-function App() {
+export default function App() {
   return (
-    <>
-      {/* Project 3 Vintage */}
-      <UserProvider>
-        <ProductProvider>
-          <CartProvider>
-            {/* <Vintage /> */}
-            <Clock />
-          </CartProvider>
-        </ProductProvider>
-      </UserProvider>
-    </>
+    <Router>
+      <Header />
+      <Alert />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/cart'>
+          <Cart />
+        </Route>
+        <PrivateRoute path='/checkout' name='jibon' msg='hai'>
+          <Checkout />
+        </PrivateRoute>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route exact path='/products'>
+          <Products />
+        </Route>
+        <Route path='/products/:id' children={<ProductDetails />}></Route>
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-// import "./index.css"; //Course 1
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; //Course 1
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
+import ProductProvider from './context/products';
+import { CartProvider } from './context/cart';
+import { UserProvider } from './context/user';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <ProductProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </ProductProvider>
+    </UserProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
